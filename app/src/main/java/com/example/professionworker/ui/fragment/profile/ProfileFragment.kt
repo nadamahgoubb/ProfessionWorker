9d3a446d2723d89674d214f4e3a99b4e7d54be35
nada <nada.moawaad@gmail.com>
2022-12-26 12:32:23 +0200

    profile , delete dialog,

--- app/src/main/java/com/example/professionworker/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/professionworker/ui/fragment/profile/ProfileFragment.kt
@@ -1,60 +1,84 @@
 package com.example.professionworker.ui.fragment.profile
 
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.fragment.findNavController
 import com.example.professionworker.R
+import com.example.professionworker.base.BaseFragment
+import com.example.professionworker.databinding.FragmentProfileBinding
+import com.example.professionworker.ui.activity.AuthActivity
+import com.example.professionworker.ui.activity.MainActivity
+import com.example.professionworker.ui.dialog.DeleteAccountSheetFragment
+import com.example.professionworker.ui.dialog.OnClick
+import com.example.professionworker.util.Constants
+import com.google.android.material.appbar.AppBarLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
+    private lateinit var parent: MainActivity
+    override fun onFragmentReady() {
+        binding.btnDelete.setOnClickListener {
+           showDeletBotttomSheetFragment()
         }
+
+        binding.ivBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
+        binding.ivMenu.setOnClickListener {
+            parent.openDrawer()
+        }
+        setupUi()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    private fun setupUi() {
+        parent = requireActivity() as MainActivity
+        parent.showBottomNav(true)
+        parent.showSideNav(true)
+        binding.cardSubscribation.tvSubscribe.setPaintFlags(  binding.cardSubscribation.tvSubscribe.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (Math.abs(verticalOffset) ==   binding.appBarLayout.getTotalScrollRange()) {
+                // If collapsed, then do this
+                binding.ivProfile.setVisibility(View.GONE);
+                binding.lytImg.setVisibility(View.GONE);
+                binding.cardSubscribation.root.setVisibility(View.GONE);
+            } else if (verticalOffset == 0) {
+                binding.lytImg.setVisibility(View.VISIBLE);
+                binding.ivProfile.setVisibility(View.VISIBLE);
+                binding.cardSubscribation.root.setVisibility(View.VISIBLE);
+            } else {
+                // Somewhere in between
+                // Do according to your requirement
+            }
+
+        })
+    }
+    fun TextView.underline() {
+        paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
+    }
+    private fun showDeletBotttomSheetFragment() {
+        DeleteAccountSheetFragment.newInstance(object : OnClick {
+            override fun onClick(choice: String) {
+                if (choice.equals(Constants.YES)) {
+                    var intent = Intent(activity, AuthActivity::class.java)
+                    intent.putExtra(Constants.Start, Constants.login)
+                    startActivity(intent)
+                    activity?.finish()
+                } else {
+
                 }
             }
+
+
+        }).show(childFragmentManager, DeleteAccountSheetFragment::class.java.canonicalName)
     }
+
 }
+
